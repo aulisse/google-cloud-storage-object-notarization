f16d649f707bab15233532d00fdac638bc67ac90
Andrea Ulisse <ulisse@ulisse-macbookpro.roam.corp.google.com>
2018-05-29 17:57:29 +0200

    OTS written in object metadata

--- function.js
+++ function.js
@@ -1,47 +1,46 @@
 'use strict';
 
 const OpenTimestamps = require('javascript-opentimestamps');
+const Storage = require('@google-cloud/storage');
+const storage = new Storage();
 
-/**
- * https://cloud.google.com/functions/docs/calling/storage
- * https://github.com/opentimestamps/javascript-opentimestamps/blob/master/README.md
- */
 exports.objectNotarization = (event, callback) => {
     const f = event.data;
     const context = event.context;
 
-    //input file information
-    console.log(`Event ${context.eventId}`);
-    console.log(`Event Type: ${context.eventType}`);
-    console.log(`Bucket: ${f.bucket}`);
-    console.log(`File: ${f.name}`);
-    console.log(`Metageneration: ${f.metageneration}`);
-    console.log(`Created: ${f.timeCreated}`);
-    console.log(`Updated: ${f.updated}`);
-    console.log(`md5hash: ${f.md5Hash}`);
-    console.log(`Function ended`);
+    //print current metadata info
+    storage
+            .bucket(f.bucket)
+            .file(f.name)
+            .getMetadata()
+            .then((data) => {
+                console.log(data);
+            })
+            .catch(err => {
+                console.error('ERROR:', err);
+            });
 
     console.log(`Starting notarization...`);
-
     const hash = Buffer.from(f.md5Hash, "hex");
     const detached = OpenTimestamps.DetachedTimestampFile.fromBytes(new OpenTimestamps.Ops.OpSHA256(), hash);
     OpenTimestamps.stamp(detached).then(() => {
-        console.log(`done`);
         console.log(detached);
-        //no need to write file. Content is logged and could be saved in file metadata
-        //const fileOts = detached.serializeToBytes();
         const infoResult = OpenTimestamps.info(detached);
         console.log(infoResult);
-
         console.log(`Notarization completed`);
+
+        console.log(`Writing OTS to object metadata...`);
+        storage
+                .bucket(f.bucket)
+                .file(f.name)
+                .setMetadata({metadata: {ots: infoResult}})
+                .then(() => {
+                    console.log(`OTS written in metadata`);
+                    callback();
+                })
+                .catch(err => {
+                    console.error('ERROR:', err);
+                });
+
     });
-
-    callback();
 };
-
-/*var propValue;
- for(var propName in f) {
- propValue = f[propName]
-
- console.log(propName,propValue);
- }*/
