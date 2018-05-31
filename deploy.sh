c800a65e3c370e34b88fff7470861a49fc7ea3f6
Andrea Ulisse <ulisse@google.com>
2018-05-31 22:26:25 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 GCS_BUCKET=bcn
 GCS_TOPIC=verification
 GCS_SUBSCRIPTION=verification
-#create pubsub topic and pubsub subscriber
+# create pubsub topic 
+# create pubsub subscriber and set ack-deadline
 gcloud beta functions deploy objectNotarization --trigger-resource $GCS_BUCKET --trigger-event google.storage.object.finalize
