c0cc8abec2381b3f07cbb56edc585962c2fc3875
Andrea Ulisse <ulisse@google.com>
2018-05-31 20:55:56 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,5 @@
 GCS_BUCKET=bcn
+GCS_TOPIC=verification
+GCS_SUBSCRIPTION=verification
+#create pubsub topic and pubsub subscriber
 gcloud beta functions deploy objectNotarization --trigger-resource $GCS_BUCKET --trigger-event google.storage.object.finalize
