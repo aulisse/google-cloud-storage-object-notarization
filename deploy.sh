09ff842383616836685dc6149e8cc19768a81adb
Andrea Ulisse <ulisse@google.com>
2018-06-08 20:07:19 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,11 @@
 GCS_BUCKET=bcn
 
-#deploy cloud functions
-gcloud beta functions deploy objectNotarization --trigger-resource $GCS_BUCKET --trigger-event google.storage.object.finalize
-gcloud beta functions deploy upgradeNotarization --trigger-resource $GCS_BUCKET --trigger-event google.storage.object.metadataUpdate
+#class must regional or multi_regional
+gsutil mb gs://$GCS_BUCKET
 
 #set bucket rule for lifecycle management
 gsutil lifecycle set lifecycle_config_file gs://$GCS_BUCKET
+
+#deploy cloud functions
+gcloud beta functions deploy objectNotarization --trigger-resource $GCS_BUCKET --trigger-event google.storage.object.finalize
+gcloud beta functions deploy upgradeNotarization --trigger-resource $GCS_BUCKET --trigger-event google.storage.object.metadataUpdate
